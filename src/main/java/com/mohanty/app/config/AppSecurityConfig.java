f57df0655abd4c85dedd1edcd932c427ed7becf6
Ashutosh Mohanty <mohantyashu.07@gmail.com>
2023-04-14 02:04:41 +0530

    Fetch user details from Database using Custom implementation of UserDetailsService

--- src/main/java/com/mohanty/app/config/AppSecurityConfig.java
+++ src/main/java/com/mohanty/app/config/AppSecurityConfig.java
@@ -2,40 +2,18 @@ package com.mohanty.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AppSecurityConfig {
 	
-	/**
-	 * Using in-memory username/password
-	 * @return
-	 */
+	
 	@Bean
 	UserDetailsService userDetailsService() {
-		UserDetails ashutosh = User
-				.withUsername("ashutosh")
-				.password("password")
-				.roles("USER")
-				.build();
-		
-		UserDetails anuja = User
-				.withUsername("anuja")
-				.password("password")
-				.roles("USER")
-				.build();
-		
-		return new InMemoryUserDetailsManager(ashutosh, anuja);
+		return new JpaUserDetailsService();
 	}
-	
-//	@Bean UserDetailsService userDetailsService() {
-//		return new JpaUserDetailsService();
-//	}
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
